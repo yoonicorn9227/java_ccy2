90cd8599d6a697f147bb583d778a443470fadec2
KOREAVC <KOREAVC@DESKTOP-LSNB6N2>
2023-09-18 18:27:49 +0900

    2023년 (3주차) 9월 15일_목요일_쇼핑몰_학생성적

--- p0915_쇼핑몰_학생성적/src/p0915_학생성적/J0915_02_학생성적프로그램.java
+++ p0915_쇼핑몰_학생성적/src/p0915_학생성적/J0915_02_학생성적프로그램.java
@@ -7,28 +7,77 @@ public class J0915_02_학생성적프로그램 {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
+		String[] title = { "번호", "이름", "국어", "영어", "수학", "합계", "평균", "등수" };
 
 		int stuCount = 1;
 
 		StuAction stuAction = new StuAction(); // 객체선언 참조변수명.변수명
 		// 무한반복
-		while (true) {
+		loop: while (true) {
 			// 메인화면 출력부분 (클래스 StuAction)
 			int choice = stuAction.mainPrint();
 
 			switch (choice) {
-			case 1: // 학생입력메소드
+			case 1: // 1.학생성적 입력 메소드
 				stuCount = stuAction.stuInput(stuCount);
 				break;
-			case 2:// 학생성적출력메소드
+			case 2: // 2.학생성적 출력 메소드
 				stuAction.stuOutput();
 				break;
-				
-				
-			case 3 : 
+
+			case 3: // 3.학생성적 수정 메소드
+				stuAction.stuUpdate();
 				break;
-			}// switch
-		} // while
+
+			case 4: // 4.학생성적 삭제 메소드
+				stuAction.stuDelete();
+				break;
+
+			case 5: // 5.학생성적 검색 메소드
+				stuAction.stuSearch();
+				break; // 전체 switch break
+
+			case 6: // 6.등수처리
+				System.out.println();
+				System.out.println("[ 학생성적 등수처리 ]");
+				System.out.printf("등수처리를 진행할까요? (0.이전페이지 이동)\n");
+				System.out.println("1. 예");
+				System.out.println("2. 아니오");
+				System.out.println("0. ▶이전페이지 이동");
+				choice = scan.nextInt();
+				switch (choice) {
+				case 1:
+					for (int i = 0; i < stuAction.list.size(); i++) {
+						int count = 1;
+						StuScore s = (StuScore) stuAction.list.get(i);
+						for (int j = 0; j < stuAction.list.size(); j++) {
+							StuScore s2 = (StuScore) stuAction.list.get(j);
+							if (s.getTotal() < s2.getTotal()) {
+								count++;
+							} // if
+						} // for2
+						s.setRank(count); // 등수입력
+					} // for
+					System.out.println("<등수처리>가 완료되었습니다!!!");
+
+				case 2:
+					System.out.println("등수처리가 취소 되었습니다.");
+					System.out.println();
+					break;
+				case 0:
+					System.out.println("이전페이지로 이동합니다.");
+					System.out.println();
+					break;
+				}// 등수처리 스위치
+				break; // case 6 break
+
+			case 0:
+				System.out.println();
+				System.out.println("[프로그램 종료]");
+				System.out.printf(">>>>>>>>프로그램을 종료합니다!<<<<<<<<<");
+				break loop;
+			}// switch 메뉴
+		} // while 메뉴 | main출력
 
 	}// MAIN
 }// CLASS
--- p0915_쇼핑몰_학생성적/src/p0915_학생성적/StuAction.java
+++ p0915_쇼핑몰_학생성적/src/p0915_학생성적/StuAction.java
@@ -16,10 +16,10 @@ public class StuAction {
 		System.out.println("[1. 학생입력]");
 		System.out.println("[2. 성적출력]");
 		System.out.println("[3. 성적수정]");
-		System.out.println("[4.성적삭제]");
-		System.out.println("[5.성적검색]");
+		System.out.println("[4. 성적삭제]");
+		System.out.println("[5. 성적검색]");
 		System.out.println("[6. 등수처리]");
-		System.out.println("[0.프로그램 종료]");
+		System.out.println("[0. 프로그램 종료]");
 		System.out.println("원하는 <번호>입력하세요.");
 		int choice = scan.nextInt();
 		return choice; // void가 int타입으로 바뀌면서 return을 붙여줘야함.
@@ -53,7 +53,7 @@ public class StuAction {
 		return stuCount;
 	}// stuInput
 
-	// 학생성적 출력메소드
+	// 2. 학생성적 출력메소드
 	void stuOutput() {
 		System.out.println();
 		System.out.println("[성적출력]");
@@ -71,5 +71,164 @@ public class StuAction {
 		System.out.println();
 
 	}// stuOutput
+
+	// 3. 학생성적 수정메소드
+	void stuUpdate() {
+		while (true) {
+			System.out.println();
+			System.out.println("[학생입력]");
+			System.out.printf("찾고자 하는 <<학생이름>>을 입력하세요. (0.이전페이지 이동)\n");
+			String name = scan.next();
+			if (name.equals("0")) {// 이전페이지 조건문
+				System.out.println(">>>이전 페이지로 이동합니다!!");
+				System.out.println();
+				break;
+			} // if
+				// 학생성적 수정
+			int count = -1; // 0,1,2....
+			for (int i = 0; i < list.size(); i++) {
+				StuScore s = (StuScore) list.get(i);
+				if (name.equals(s.getName())) {
+					count = i; // 찾은 방 번호
+					System.out.printf("%s 학생을 찾았습니다.\n", name);
+					System.out.println(" [ 수정과목 선택 ] ");
+					System.out.println("1.국어");
+					System.out.println("2.영어");
+					System.out.println("3.수학");
+					System.out.println("----------------------------");
+					System.out.println("원하는 번호를 선택하세요.");
+					int choice = scan.nextInt();
+					switch (choice) {
+					case 1:
+						System.out.println("[국어과목  수정]");
+						System.out.println("----------------------------");
+						System.out.println("현재점수 : " + s.getKor());
+						System.out.println("변경할 점수를 입력하세요.");
+						s.setKor(scan.nextInt()); // 입력 후 바로 국어점수에 저장
+						s.setTotal(s.getKor() + s.getEng() + s.getMath());// 점수를 읽은 후 바로 합계에 저장
+						s.setAvg(s.getTotal() / 3.0);
+						System.out.println("국어점수가 변경되었습니다!");
+						System.out.println();
+						break;
+
+					case 2:
+						System.out.println("[영어과목  수정]");
+						System.out.println("----------------------------");
+						System.out.println("현재점수 : " + s.getEng());
+						System.out.println("변경할 점수를 입력하세요.");
+						s.setEng(scan.nextInt()); // 입력 후 바로 영어점수에 저장
+						s.setTotal(s.getKor() + s.getEng() + s.getMath());// 점수를 읽은 후 바로 합계에 저장
+						s.setAvg(s.getTotal() / 3.0);
+						System.out.println("영어점수가 변경되었습니다!");
+						System.out.println();
+						break;
+
+					case 3:
+						System.out.println("[수학과목  수정]");
+						System.out.println("----------------------------");
+						System.out.println("현재점수 : " + s.getMath());
+						System.out.println("변경할 점수를 입력하세요.");
+						s.setMath(scan.nextInt()); // 입력 후 바로 수학점수에 저장
+						s.setTotal(s.getKor() + s.getEng() + s.getMath());// 점수를 읽은 후 바로 합계에 저장
+						s.setAvg(s.getTotal() / 3.0);
+						System.out.println("수학점수가 변경되었습니다!");
+						System.out.println();
+						break;
+					}// switch 성적수정
+				} // if
+			} // for
+			if (count == -1) {
+				System.out.printf("※※찾고자 하는 %s 학생이 없습니다. ※※다시입력하세요.\n", name);
+				System.out.println();
+			} // if
+
+		} // while 수정
+	}// stuUpdate
+
+	// 4. 학생성적 삭제 메소드
+	void stuDelete() {
+		while (true) {
+			System.out.println();
+			System.out.println("[학생성적 삭제]");
+			System.out.printf("삭제하고자 하는 <<학생이름>>을 입력하세요. (0.이전페이지 이동)\n");
+			String name = scan.next();
+			if (name.equals("0")) {// 이전페이지 조건문
+				System.out.println(">>>이전 페이지로 이동합니다!!");
+				System.out.println();
+				break;
+			} // if
+			int count = -1; // 0,1,2....
+			for (int i = 0; i < list.size(); i++) {
+				StuScore s = (StuScore) list.get(i);
+				if (name.equals(s.getName())) {
+					count = i; // 찾은 방 번호
+					System.out.printf("%s 학생을 찾았습니다.\n", name);
+					System.out.println(" [ 학생성적 삭제 선택 ] ");
+					System.out.println("1.예");
+					System.out.println("2.아니오");
+					System.out.println("----------------------------");
+					System.out.println("원하는 번호를 선택하세요.");
+					int choice = scan.nextInt();
+					switch (choice) {
+					case 1:
+						list.remove(i);
+						System.out.printf("%s학생성적이 <<삭제>> 되었습니다!", name);
+						System.out.println();
+						break;
+
+					case 2:
+						System.out.printf("%s학생성적이 <<취소>> 되었습니다!", name);
+						System.out.println();
+						break;
+					}// switch 성적삭제
+				} // if
+			} // for
+			if (count == -1) {
+				System.out.printf("※※찾고자 하는 %s 학생이 없습니다. ※※다시입력하세요.\n", name);
+				System.out.println();
+			} // if
+		} // while 삭제
+	}// stuDelete
+
+	// 5. 학생성적 검색 메소드
+	void stuSearch() {
+		while (true) {
+
+			System.out.println();
+			System.out.println("[학생성적 검색]");
+			System.out.printf("찾고자 하는 <<학생이름>>을 입력하세요. (0.이전페이지 이동)\n");
+			String name = scan.next();
+			if (name.equals("0")) {// 이전페이지 조건문
+				System.out.println(">>>이전 페이지로 이동합니다!!");
+				System.out.println();
+				break;
+			} // if
+			int count = -1; // 0,1,2....
+			for (int i = 0; i < list.size(); i++) {
+				StuScore s = (StuScore) list.get(i);
+				if (name.equals(s.getName())) {
+					count = i; // 찾은 방 번호
+					System.out.printf("%s 학생을 찾았습니다.\n", name);
+
+					System.out.println();
+					System.out.printf("[%s 학생성적 출력]\n", name);
+					for (int j = 0; j < title.length; j++) {
+						System.out.printf("%s\t", title[j]);
+					}
+					System.out.println();
+					System.out.println("------------------------------------------------------------------------");
+					System.out.printf("%d\t %s\t %d\t %d\t %d\t %d\t %.2f\t %d\n ", s.getStuNo(), s.getName(),
+							s.getKor(), s.getEng(), s.getMath(), s.getTotal(), s.getAvg(), s.getRank());
+					System.out.println("------------------------------------------------------------------------");
+					System.out.println();
+					break; // break for
+				} // if
+			} // for
+			if (count == -1) {
+				System.out.printf("※※찾고자 하는 %s 학생이 없습니다. ※※다시입력하세요.\n", name);
+				System.out.println();
+			} // if
+		} // 검색 while
+	}// stuSearch
 
 }// CLASS
